fcb1fb1116281374ac5b5fe44307f75c00c5ed9c
Davide Bortolami <d@fermiumlabs.com>
2017-08-07 09:56:29 +0200

    improvement to run script, added run-one script

--- run-one.py
+++ run-one.py
@@ -9,7 +9,15 @@ masterdialog = Masterdialog(dialog="dialog")
 masterdialog.set_background_title(
     "Fermium LABS testing procedure - Hall Effect apparatus")
 
-testspaths = glob.glob("./tests/*/test.py")
+testtorun = input("Which test do you want to run? ")
+try:
+    int(testtorun)
+except ValueError:
+    print("Please insert an integer number")
+    sys.exit(0)
+
+
+testspaths = glob.glob("./tests/" + testtorun + "*/test.py")
 testspaths.sort()
 
 # Fundamental test dict
@@ -17,28 +25,14 @@ tests = {}
 for testpath in testspaths:
     tests[os.path.dirname(testpath).split("/").pop()] = {}
     tests[os.path.dirname(testpath).split("/").pop()
-                          ]["path"] = os.path.abspath(testpath)
+          ]["path"] = os.path.abspath(testpath)
     tests[os.path.dirname(testpath).split("/").pop()]["status"] = "not yet run"
 del testspaths
 
 
-def show_master_dialog():
-    """show quick summary of the progress"""
-    def calculate_progress_percentage(d):
-        """Calculates progress of the testing operation"""
-        successcounter = 0
-        for test in d:
-            if d[test]["status"] is "success":
-                successcounter += 1
-        totalcounter = 0
-        for test in d:
-            totalcounter += 1
-        return int(successcounter / totalcounter * 100)
-
-    percent = calculate_progress_percentage(tests)
-    masterdialog.mixedgauge("TEST SUMMARY - Press ENTER key to continue...",
-                            percent=percent, elements=[(k, v["status"]) for k, v in tests.items()])
-    input()
+if len(tests) == 0:
+    print("No test found")
+    sys.exit(0)
 
 
 def signal_handler(signal, frame):
@@ -57,18 +51,14 @@ for TESTNAME in tests:
         tests[TESTNAME]["path"]) + "/" + "assets/*"))
     for f in files:
         os.remove(f)
-    
+            
     # clear screen
     print(chr(27) + "[2J")
-    show_master_dialog()
+    
     testfile = open(tests[TESTNAME]["path"], "r")
     # while tests[TESTNAME]["status"] != "success":
     exec(testfile.read())
 
 
-show_master_dialog()
-
-
 print(chr(27) + "[2J")
 sys.exit(0)
-        
